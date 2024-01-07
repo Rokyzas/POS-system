a699033dd712ac8b69333138d1e2c7599114f743
Linas490 <113028160+Linas490@users.noreply.github.com>
2024-01-07 16:52:36 +0200

    Added booking, staff and TimeSlot classes

---
+++ POS/POS/Models/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace POS.Models
+{
+    public class Booking
+    {
+        public int id { get; set; } // Corresponds to SERIAL PRIMARY KEY
+        public int TimeSlotID { get; set; } // INT NOT NULL
+        public int ServiceID { get; set; } // INT NOT NULL
+        public string Status { get; set; } // VARCHAR(255) with CHECK constraint
+
+    }
+}
