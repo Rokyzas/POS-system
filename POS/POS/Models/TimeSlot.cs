c03db858b2a913d6e7e090326c1cd2d1b06ca4b7
Linas490 <113028160+Linas490@users.noreply.github.com>
2024-01-09 19:36:07 +0200

    booking and TimeSlot status enums

--- POS/POS/Models/TimeSlot.cs
+++ POS/POS/Models/TimeSlot.cs
@@ -1,11 +1,22 @@
-﻿namespace POS.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace POS.Models
 {
+    public enum TimeSlotStatus
+    {
+        Available,
+        Unavailable
+    }
+
     public class TimeSlot
     {
         public int id { get; set; } // Corresponds to SERIAL PRIMARY KEY
         public DateTime StartDate { get; set; } // TIMESTAMP NOT NULL
         public DateTime EndDate { get; set; } // TIMESTAMP NOT NULL
+
+        [ForeignKey("Staff")]
         public int StaffID { get; set; } // INT NOT NULL
-        public string Status { get; set; } // VARCHAR(255) with CHECK constraint
+        public TimeSlotStatus Status { get; set; }
     }
 }
+
